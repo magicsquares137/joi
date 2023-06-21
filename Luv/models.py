d91aee9f22353f2e85dbff23fbf7e81f21fe251b
Muhammad Omer Saleem <omer.knysys@gmail.com>
2023-06-21 15:56:29 +0500

    Added new model for profile Created signals for user and profile model Migration file of categories and profile

--- Luv/models.py
+++ Luv/models.py
@@ -3,12 +3,25 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.conf import settings
 import yaml
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 with open(os.path.join(settings.BASE_DIR, "config.yml"), "r") as file:
     CONFIG = yaml.safe_load(file)
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    main_Img = models.ImageField(upload_to="profile_images/", null=True, blank=True)
 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Categories(models.Model):
     name = models.CharField(
