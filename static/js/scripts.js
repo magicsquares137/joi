c308e1f942bee0dbfd4f2007e4a0ff44be125a51
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2023-06-22 18:15:29 +0500

    Site UI Fixes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,77 +1,68 @@
-
-
 // Cards Slider JS
-function isSmallScreen() {
+$(document).ready(function () {
+  function isSmallScreen() {
   return window.innerWidth <= 1024;
-}
-function initOwlCarousel() {
+  }
+  function initOwlCarousel() {
   $('.cards-slider').owlCarousel({
-    margin: 17,
-    loop: false,
-    autoWidth: true,
-    responsiveClass: true,
-    items: 4,
-    nav: true,
+  margin: 17,
+  loop: false,
+  autoWidth: true,
+  responsiveClass: true,
+  items: 4,
+  nav: true,
   });
-}
-
-function handleResize() {
+  }
+  
+  function handleResize() {
   if (!isSmallScreen()) {
-    initOwlCarousel();
+  initOwlCarousel();
   } else {
-    $('.cards-slider').trigger('destroy.owl.carousel'); // Destroy the carousel on small screens
+  $('.cards-slider').trigger('destroy.owl.carousel'); // Destroy the carousel on small screens
   }
-}
-
-window.addEventListener('resize', handleResize);
-handleResize();
-
-
-
-
-
-
-
-// Form Password Eye
-
-// Pass Eye add html
-
-// Show and hide eye
-$(document).ready(function () {
-  $('#id_password1').addClass('pass-field');
-  $('#id_password').addClass('pass-field');
-  $('.pass-field').after('<div class="password-eye"><i class="fa-solid fa-eye eye"></i></div>');
-  $('.eye').click(function () {
-    $(this).toggleClass('fa-eye-slash');
-    var passwordInput = $('.pass-field');
-    var type = passwordInput.attr('type') === 'password' ? 'text' : 'password';
-    passwordInput.attr('type', type);
-  });
-});
- 
-
-// Chat Textarea js
-$(document).ready(function () {
-$('textarea#id_message').attr('placeholder', 'Type a message').removeAttr('cols').removeAttr('rows').removeAttr('maxlength');
-});
-
-
-
-
-// Search Pop
-  function openNav() {
-    document.getElementById("mobile-search").style.height = "100%";
   }
-  function closeNav() {
-    document.getElementById("mobile-search").style.height = "0%";
-  }
-
-
+  
+  window.addEventListener('resize', handleResize);
+  handleResize();
+  
+  
   // Filter Category JS
-$('.owl-carousel').owlCarousel({
+  $('.owl-carousel').owlCarousel({
   margin:15,
   loop:false,
   autoWidth:true,
   items:1,
   nav:true,
-});
+  });
+  
+  // Chat Textarea js
+  $(document).ready(function () {
+  $('textarea#id_message').attr('placeholder', 'Type a message').removeAttr('cols').removeAttr('rows').removeAttr('maxlength');
+  });
+  
+  
+  });
+  
+  // Form Password Eye
+  // Pass Eye add html
+  // Show and hide eye
+  $(document).ready(function () {
+  $('#id_password1').addClass('pass-field');
+  $('#id_password').addClass('pass-field');
+  $('.pass-field').after('<div class="password-eye"><i class="fa-solid fa-eye eye"></i></div>');
+  $('.eye').click(function () {
+  $(this).toggleClass('fa-eye-slash');
+  var passwordInput = $('.pass-field');
+  var type = passwordInput.attr('type') === 'password' ? 'text' : 'password';
+  passwordInput.attr('type', type);
+  });
+  });
+  
+  
+  // Search Pop
+  function openNav() {
+  document.getElementById("mobile-search").style.height = "100%";
+  }
+  function closeNav() {
+  document.getElementById("mobile-search").style.height = "0%";
+  }
