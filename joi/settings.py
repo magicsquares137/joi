5aa49ade33836654698466ddeb81146d22958ee9
Muhammad Omer Saleem <omer.knysys@gmail.com>
2023-06-23 10:58:27 +0500

    Configured smtp for password reset

--- joi/settings.py
+++ joi/settings.py
@@ -138,3 +138,10 @@ LOGIN_URL = "login"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'magicsquare.ai@gmail.com'
+EMAIL_HOST_PASSWORD = 'rbpinsmirxvluomc'
+EMAIL_USE_TLS = True
